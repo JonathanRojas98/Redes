ef910fa8004bd5b79b0ae2213f320688b70bf203
Jonathan <jonathanrm117@gmail.com>
2021-09-01 21:19:01 -0500

    Feat: comunicacion unidireccional funcionando

--- Tarea 1 MiniDrive/FlujoArchivo/src/CEnvia.java
+++ Tarea 1 MiniDrive/FlujoArchivo/src/CEnvia.java
@@ -6,56 +6,90 @@ import javax.swing.JFileChooser;
  * @author axele
  */
 public class CEnvia {
+    static DataOutputStream dos;
+    static DataInputStream dis;
+    static Socket cl;
+    
+    static void enviar_directorios(File[] directorios){
+        for (File directorio : directorios) {
+            if (directorio.isDirectory()) {
+                String path = directorio.getAbsolutePath() + "\\";
+                enviar_directorios(directorio.listFiles(File::isDirectory));
+                try{
+                    dos.writeUTF(path);
+                    dos.flush();
+                } catch(Exception e){
+                    e.printStackTrace();
+                }
+                System.out.println(path);
+            }
+        }
+    }
+    
+    static void enviar_archivos(File[] archivos){
+        for (File archivo : archivos) {
+            String path = archivo.getAbsolutePath();
+            if (archivo.isDirectory()) {
+                enviar_archivos(archivo.listFiles());
+            } else {
+                try{
+                    dis = new DataInputStream(new FileInputStream(path));
+                    long tam = archivo.length();
+                    System.out.println("Tamano del archivo a enviar es de: " + tam);
+                    System.out.println("El nombre del archivo es: " + archivo.getName());
+                    dos.writeUTF(path);
+                    dos.flush();
+                    dos.writeLong(tam);
+                    dos.flush();
+                    long enviados = 0;
+                    int l=0, porcentaje=0;
+                    while(enviados < tam){
+                        byte[] b = (tam < 1500) ? new byte[(int)tam] : new byte[1500];
+                        //byte[] b = new byte[1500];
+                        l=dis.read(b);
+                        System.out.println("enviados: "+l);
+                        dos.write(b,0,l);// dos.write(b);
+                        dos.flush();
+                        enviados = enviados + l;
+                        porcentaje = (int)((enviados*100)/tam);
+                        System.out.print("\rEnviado el "+porcentaje+" % del archivo");
+                    }//while
+                    System.out.println("\nArchivo enviado..");
+                } catch(Exception e){
+                    e.printStackTrace();
+                }
+                System.out.println(path);
+            }
+        }
+    }
+    
+    
     public static void main(String[] args){
         try{
             int pto = 8000;
             String dir = "127.0.0.1";
-            Socket cl = new Socket(dir,pto);
-            System.out.println("Conexion con servidor establecida.. lanzando FileChooser..");
-            JFileChooser jf = new JFileChooser();
+            cl = new Socket(dir,pto);
+            System.out.println("Conexion con servidor establecida..");
+            
+            
+            //ESTO ES PARA ENVIAR ARCHIVOS
+           
+            JFileChooser jf = new JFileChooser(new File("."));
             jf.setMultiSelectionEnabled(true);
+            jf.setFileSelectionMode(JFileChooser.FILES_AND_DIRECTORIES);
             int r = jf.showOpenDialog(null);
             if(r==JFileChooser.APPROVE_OPTION){
-                File f = jf.getSelectedFile();
-                //File[] f = jf.getSelectedFiles();
-                 File ff= new File("");
-                 String z = ff.getAbsolutePath()+"\\";
-                 System.out.println("Ruta:"+z);
-                 File ff2 = new File(z);
-                File[] archivos = ff2.listFiles();
-                System.out.println("Hay "+archivos.length+"archivos");
-            for(int i=0;i<archivos.length;i++){
-                String xx = archivos[i].getAbsolutePath();
-                xx = (archivos[i].isDirectory())? xx+ "/":xx;
-                System.out.println(xx); 
-            }//
-                String nombre = f.getName();
-                String path = f.getAbsolutePath();
-                long tam = f.length();
-                System.out.println("Preparandose pare enviar archivo "+path+" de "+tam+" bytes\n\n");
-                DataOutputStream dos = new DataOutputStream(cl.getOutputStream());
-                DataInputStream dis = new DataInputStream(new FileInputStream(path));
-                dos.writeUTF(nombre);
-                dos.flush();
-                dos.writeLong(tam);
-                dos.flush();
-                long enviados = 0;
-                int l=0,porcentaje=0;
-                while(enviados<tam){
-                    byte[] b = new byte[1500];
-                    l=dis.read(b);
-                    System.out.println("enviados: "+l);
-                    dos.write(b,0,l);// dos.write(b);
-                    dos.flush();
-                    enviados = enviados + l;
-                    porcentaje = (int)((enviados*100)/tam);
-                    System.out.print("\rEnviado el "+porcentaje+" % del archivo");
-                }//while
-                System.out.println("\nArchivo enviado..");
-                dis.close();
+                dos = new DataOutputStream(cl.getOutputStream());
+                File[] archivos = jf.getSelectedFiles();
+                enviar_directorios(archivos);
+                enviar_archivos(archivos);
+                dos.writeUTF("fin");
                 dos.close();
                 cl.close();
-            }//if
+            }
+            
+            //PARA ENVIAR ARCHIVOS
+            
         }catch(Exception e){
             e.printStackTrace();
         }//catch
--- Tarea 1 MiniDrive/FlujoArchivo/src/SRecibe.java
+++ Tarea 1 MiniDrive/FlujoArchivo/src/SRecibe.java
@@ -1,5 +1,6 @@
 import java.net.*;
 import java.io.*;
+import java.util.regex.Pattern;
 
 /**
  *
@@ -14,38 +15,55 @@ public class SRecibe {
           s.setReuseAddress(true);
           System.out.println("Servidor iniciado esperando por archivos..");
           
-          File f = new File("");
-          String ruta = f.getAbsolutePath();
-          String carpeta="archivos";
-          String ruta_archivos = ruta+"\\"+carpeta+"\\";
-          System.out.println("ruta:"+ruta_archivos);
-          File f2 = new File(ruta_archivos);
-          f2.mkdirs();
-          f2.setWritable(true);
           for(;;){
               Socket cl = s.accept();
               System.out.println("Cliente conectado desde "+cl.getInetAddress()+":"+cl.getPort());
               DataInputStream dis = new DataInputStream(cl.getInputStream());
-              String nombre = dis.readUTF();
-              long tam = dis.readLong();
-              System.out.println("Comienza descarga del archivo "+nombre+" de "+tam+" bytes\n\n");
-              DataOutputStream dos = new DataOutputStream(new FileOutputStream(ruta_archivos+nombre));
-              long recibidos=0;
-              int l=0, porcentaje=0;
-              while(recibidos<tam){
-                  byte[] b = new byte[1500];
-                  l = dis.read(b);
-                  System.out.println("leidos: "+l);
-                  dos.write(b,0,l);
-                  dos.flush();
-                  recibidos = recibidos + l;
-                  porcentaje = (int)((recibidos*100)/tam);
-                  System.out.print("\rRecibido el "+ porcentaje +" % del archivo");
-              }//while
-              System.out.println("Archivo recibido..");
-              dos.close();
-              dis.close();
-              cl.close();
+              
+              
+              
+              
+              //PARA RECIBIR DATOS!
+              while(true){
+                String nombre = dis.readUTF();
+                if(nombre.compareToIgnoreCase("fin") == 0){
+                    System.out.println("Cliente desconectado");
+                    dis.close();
+                    cl.close();
+                    break;
+                } else if (nombre.substring(nombre.length() - 1).contains("\\")){
+                    String directorio = nombre.replace("\\", "\\\\").split("archivos\\\\")[1];
+                    directorio = directorio.replace("\\\\", "/");
+                    directorio = directorio.substring(1);
+                    new File("./archivosServer/" + directorio).mkdirs();
+                    
+                    //System.out.println(directorio);
+                } else{
+                    long tam = dis.readLong();
+                    nombre = nombre.replace("\\", "\\\\").split("archivos\\\\")[1];
+                    nombre = nombre.replace("\\\\", "/");
+                    nombre = nombre.substring(1);
+                    System.out.println(nombre);
+                    System.out.println("Comienza descarga del archivo "+nombre+" de "+tam+" bytes\n\n");
+                    DataOutputStream dos = new DataOutputStream(new FileOutputStream("./archivosServer/" + nombre));
+                    long recibidos=0;
+                    int l=0, porcentaje=0;
+                    while(recibidos < tam){
+                        byte[] b = (tam < 1500) ? new byte[(int)tam] : new byte[1500];
+                        l = dis.read(b);
+                        System.out.println("leidos: "+l);
+                        dos.write(b,0,l);
+                        dos.flush();
+                        recibidos = recibidos + l;
+                        porcentaje = (int)((recibidos*100)/tam);
+                        System.out.print("\rRecibido el "+ porcentaje +" % del archivo");
+                    }//while
+                    System.out.println("Archivo recibido..");
+                    //
+                }
+              }
+              //PARA ENVIAR DATOS!
+              
           }//for
           
       }catch(Exception e){
